07cbc348936e29bde71ff0c4c2420bcca9353562
Daniel Cesario <danielatcesario@gmail.com>
2021-04-11 11:25:37 -0300

    Create new main activity and change country activity

--- app/src/main/java/com/example/dadoscovidbrasil/ui/MainActivity.kt
+++ app/src/main/java/com/example/dadoscovidbrasil/ui/MainActivity.kt
@@ -1,39 +1,13 @@
 package com.example.dadoscovidbrasil.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.example.dadoscovidbrasil.R
-import com.example.dadoscovidbrasil.models.CountrySummary
-import com.example.dadoscovidbrasil.repositories.Covid19BrazilServiceAPI
-import com.example.dadoscovidbrasil.services.Covid19BrazilService
-import com.example.dadoscovidbrasil.usecases.GetCountrySummary
-import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.ext.android.get
-import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    private val nf: NumberFormat = NumberFormat.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val covid19BrazilServiceAPI: Covid19BrazilServiceAPI = get()
-        val covid19BrazilService: Covid19BrazilService = covid19BrazilServiceAPI.getCovid19BrazilService()
-        val getCountrySummary = GetCountrySummary(covid19BrazilService)
-
-        getCountrySummary.execute("brazil")
-        getCountrySummary.countrySummaryData.observe(this, Observer { countrySummary -> fillMainCard(countrySummary)})
-    }
-
-    fun fillMainCard(countrySummary: CountrySummary) {
-        Log.d("MainActivity", countrySummary.data.toString())
-        country_value.text = countrySummary.data.country
-        cases_value.text = nf.format(countrySummary.data.cases)
-        confirmed_value.text = nf.format(countrySummary.data.confirmed)
-        deaths_value.text = nf.format(countrySummary.data.deaths)
-        recovered_value.text = nf.format(countrySummary.data.recovered)
     }
 }
